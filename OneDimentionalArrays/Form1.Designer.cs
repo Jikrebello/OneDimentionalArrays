0c6176e94dff133a0fc84ed12f3481c36f121139
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:37:27 +0200

    Add files via upload

---
+++ OneDimentionalArrays/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace OneDimentionalArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDisplayArrayIndex = new System.Windows.Forms.Button();
+            this.TxtDisplay = new System.Windows.Forms.TextBox();
+            this.BtnPassingMarks = new System.Windows.Forms.Button();
+            this.BtnMathsAndFrench = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnDisplayArrayIndex
+            // 
+            this.BtnDisplayArrayIndex.Location = new System.Drawing.Point(12, 12);
+            this.BtnDisplayArrayIndex.Name = "BtnDisplayArrayIndex";
+            this.BtnDisplayArrayIndex.Size = new System.Drawing.Size(148, 50);
+            this.BtnDisplayArrayIndex.TabIndex = 0;
+            this.BtnDisplayArrayIndex.Text = "Display Array Columns";
+            this.BtnDisplayArrayIndex.UseVisualStyleBackColor = true;
+            this.BtnDisplayArrayIndex.Click += new System.EventHandler(this.BtnDisplayArrayIndex_Click);
+            // 
+            // TxtDisplay
+            // 
+            this.TxtDisplay.Location = new System.Drawing.Point(166, 12);
+            this.TxtDisplay.Multiline = true;
+            this.TxtDisplay.Name = "TxtDisplay";
+            this.TxtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtDisplay.Size = new System.Drawing.Size(265, 253);
+            this.TxtDisplay.TabIndex = 1;
+            // 
+            // BtnPassingMarks
+            // 
+            this.BtnPassingMarks.Location = new System.Drawing.Point(12, 68);
+            this.BtnPassingMarks.Name = "BtnPassingMarks";
+            this.BtnPassingMarks.Size = new System.Drawing.Size(148, 50);
+            this.BtnPassingMarks.TabIndex = 2;
+            this.BtnPassingMarks.Text = "View Passing Marks";
+            this.BtnPassingMarks.UseVisualStyleBackColor = true;
+            this.BtnPassingMarks.Click += new System.EventHandler(this.BtnPassingMarks_Click);
+            // 
+            // BtnMathsAndFrench
+            // 
+            this.BtnMathsAndFrench.Location = new System.Drawing.Point(12, 124);
+            this.BtnMathsAndFrench.Name = "BtnMathsAndFrench";
+            this.BtnMathsAndFrench.Size = new System.Drawing.Size(148, 49);
+            this.BtnMathsAndFrench.TabIndex = 3;
+            this.BtnMathsAndFrench.Text = "View Maths and French";
+            this.BtnMathsAndFrench.UseVisualStyleBackColor = true;
+            this.BtnMathsAndFrench.Click += new System.EventHandler(this.LoadInMathsAndFrench);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 86);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "View Maths and French Top Achievers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ViewTopAchieverTotal);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 282);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnMathsAndFrench);
+            this.Controls.Add(this.BtnPassingMarks);
+            this.Controls.Add(this.TxtDisplay);
+            this.Controls.Add(this.BtnDisplayArrayIndex);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDisplayArrayIndex;
+        private System.Windows.Forms.TextBox TxtDisplay;
+        private System.Windows.Forms.Button BtnPassingMarks;
+        private System.Windows.Forms.Button BtnMathsAndFrench;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OneDimentionalArrays/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+
+namespace OneDimentionalArrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * 1. a) Load in ten numbers (12,14,23,2,56,3,78,88,143,54) into an array called num.
+         * b) Display, in two columns, the index and the contents of each element in the array.
+         * As a result , you should understand the distinction between the array subscript
+         * (index) and the contents of the array .
+         */
+        private void BtnDisplayArrayIndex_Click(object sender, EventArgs e)
+        {
+            int[] marks = { 12, 14, 23, 2, 56, 3, 78, 88, 143, 54 };
+            TxtDisplay.Text = "";
+
+            TxtDisplay.Text += "Index:\tValue:" + Environment.NewLine;
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                TxtDisplay.Text += Array.IndexOf(marks, marks[i]).ToString() + "\t" + marks[i].ToString() + Environment.NewLine;
+            }
+        }
+
+        /*
+         * 2. a) Load in eight marks into an array called mark.
+         * b) Display only the passing marks
+         * c) Calculate the sum of every other mark.
+         * d) Calculate the sum of the passing marks only.
+         */
+        private void BtnPassingMarks_Click(object sender, EventArgs e)
+        {
+            int[] marks = { 50, 60, 76, 43, 23, 66, 56, 78 };
+            float sumOfPassedMarks = 0;
+            int totalOfPassedMarks = 0;
+
+            float sumOfFailedMarks = 0;
+            int totalOfFaileddMarks = 0;
+            TxtDisplay.Text = "";
+
+            TxtDisplay.Text += "Passed Marks:" + Environment.NewLine;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] >= 50)
+                {
+                    totalOfPassedMarks++;
+                    sumOfPassedMarks += marks[i];
+                    TxtDisplay.Text += marks[i] + Environment.NewLine;
+                }
+            }
+
+            TxtDisplay.Text += Environment.NewLine + "Failed Marks:" + Environment.NewLine;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] < 50)
+                {
+                    totalOfFaileddMarks++;
+                    sumOfFailedMarks += marks[i];
+                    TxtDisplay.Text += marks[i] + Environment.NewLine;
+                }
+            }
+
+            TxtDisplay.Text += Environment.NewLine + "Average For Passed Marks: " + Math.Round(sumOfPassedMarks / totalOfPassedMarks, 2);
+            TxtDisplay.Text += Environment.NewLine + "Average For Failed Marks: " + Math.Round(sumOfFailedMarks / totalOfFaileddMarks, 2);
+        }
+
+        /*
+         * 3. St. Jean de Brebeuf is conducting a survey of their students to find out if doing well in
+         * Math has anything to do with doing well in French.
+         * a) Write an event procedure to load in 25 pairs of marks. The first mark in each pair
+         * is the Math mark, the second is the French mark.
+         * b) Write a separate event procedure that counts all those students who have 80% or
+         * higher in both Math and French and displays the final count .
+         */
+        private List<string> frenchList = new List<string>();
+        private List<string> mathList = new List<string>();
+        private void LoadInMathsAndFrench(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + @"\Math_and_French_Marks.txt";
+
+            // Gets the marks from the file and places them into the respective list based off whether the line is odd or even (French is even, Maths is odd)
+            using (StreamReader reader = new StreamReader(path))
+            {
+                bool lineNumberEven = false; // french marks are odd
+
+                while (!reader.EndOfStream)
+                {
+                    if (!lineNumberEven)
+                    {
+                        frenchList.Add(reader.ReadLine());
+                        lineNumberEven = !lineNumberEven;
+                    }
+                    else
+                    {
+                        mathList.Add(reader.ReadLine());
+                        lineNumberEven = !lineNumberEven;
+                    }
+                }
+
+                reader.Close();
+            }
+
+            // Display the French and Maths marks in the text box
+            string header = "French marks:\tMaths Marks:" + Environment.NewLine;
+            TxtDisplay.Text = "";
+            TxtDisplay.Text += header;
+
+            for (int i = 0; i < frenchList.Count; i++)
+            {
+                TxtDisplay.Text += frenchList[i].ToString() + "\t\t" + mathList[i].ToString() + Environment.NewLine;
+            }
+        }
+        private void ViewTopAchieverTotal(object sender, EventArgs e)
+        {
+            int totalAchievers = 0;
+
+            for (int i = 0; i < frenchList.Count; i++)
+            {
+                if (Convert.ToInt32(frenchList[i]) >= 80 && Convert.ToInt32(mathList[i]) >= 80)
+                {
+                    totalAchievers++;
+                }
+            }
+
+            TxtDisplay.Text = "";
+            TxtDisplay.Text = "Total Achievers:\t" + totalAchievers;
+        }
+    }
+}
